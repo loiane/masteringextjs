4597b551cb9b74a98348c5fa2aab7a7b94b431c2
Loiane Groner <me@loiane.com>
2014-09-22 22:00:47 -0300

    added chapter 4

---
+++ build/development/Packt/resources/locale/pt_BR.js
@@ -0,0 +1,17 @@
+translations = {
+	login: "Login",
+	user: "Usuário",
+	password: "Senha",
+
+	cancel: "Cancelar",
+	submit: "Enviar",
+	logout: 'Logout',
+
+	capsLockTitle: 'Caps Lock está ativada',
+	capsLockMsg1: 'Se Caps lock estiver ativado, isso pode fazer ',
+	capsLockMsg2: 'com que você digite a senha incorretamente.',
+	capsLockMsg3: 'Você deve pressionar a tecla Caps lock para ',
+	capsLockMsg4: 'desativá-la antes de digitar a senha.',
+
+    home: 'Início'
+};

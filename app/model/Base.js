dca7ee1b91c4535ebdf162dc2ce859a5e6381c20
Loiane Groner <me@loiane.com>
2014-10-11 10:46:02 -0300

    created Base Model for app

---
+++ app/model/Base.js
@@ -0,0 +1,37 @@
+Ext.define('Packt.model.Base', {
+    extend: 'Ext.data.Model',
+
+    requires: [
+        'Packt.util.Util'
+    ],
+
+    schema: {
+        namespace: 'Packt.model',
+        urlPrefix: 'php',
+        proxy: {
+            type: 'ajax',
+            api :{
+                read : '{prefix}/{entityName:lowercase}/list.php',
+                create: '{prefix}/{entityName:lowercase}/create.php',
+                update: '{prefix}/{entityName:lowercase}/update.php',
+                destroy: '{prefix}/{entityName:lowercase}/destroy.php'
+            },
+            reader: {
+                type: 'json',
+                rootProperty: 'data'
+            },
+            writer: {
+                type: 'json',
+                writeAllFields: true,
+                encode: true,
+                rootProperty: 'data',
+                allowSingle: false
+            },
+            listeners: {
+                exception: function(proxy, response, operation){
+                    Packt.util.Util.showErrorMsg(response.responseText);
+                }
+            }
+        }
+    }
+});

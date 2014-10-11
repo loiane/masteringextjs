0f612d4e70f594b76195c10f8b180969d2c8190e
Loiane Groner <me@loiane.com>
2014-10-11 10:43:36 -0300

    added Views - chapter 7

---
+++ app/view/staticData/BaseGrid.js
@@ -0,0 +1,95 @@
+Ext.define('Packt.view.staticData.BaseGrid', {
+    extend: 'Ext.ux.LiveSearchGridPanel',
+    xtype: 'staticdatagrid',
+
+    requires: [
+        'Packt.util.Glyphs'
+    ],
+
+    columnLines: true,
+    viewConfig: {
+        stripeRows: true
+    },
+
+    initComponent: function() {
+        var me = this;
+
+        me.selModel = {
+            selType: 'cellmodel'
+        };
+
+        me.plugins = [
+            {
+                ptype: 'cellediting',
+                clicksToEdit: 1,
+                pluginId: 'cellplugin'
+            },
+            {
+                ptype: 'gridfilters'
+            }
+        ];
+
+        me.dockedItems = [
+            {
+                xtype: 'toolbar',
+                dock: 'top',
+                itemId: 'topToolbar',
+                items: [
+                    {
+                        xtype: 'button',
+                        itemId: 'add',
+                        text: 'Add',
+                        glyph: Packt.util.Glyphs.getGlyph('add')
+                    },
+                    {
+                        xtype: 'tbseparator'
+                    },
+                    {
+                        xtype: 'button',
+                        itemId: 'save',
+                        text: 'Save Changes',
+                        glyph: Packt.util.Glyphs.getGlyph('saveAll')
+                    },
+                    {
+                        xtype: 'button',
+                        itemId: 'cancel',
+                        text: 'Cancel Changes',
+                        glyph: Packt.util.Glyphs.getGlyph('cancel')
+                    },
+                    {
+                        xtype: 'tbseparator'
+                    },
+                    {
+                        xtype: 'button',
+                        itemId: 'clearFilter',
+                        text: 'Clear Filters',
+                        glyph: Packt.util.Glyphs.getGlyph('clearFilter')
+                    }
+                ]
+            }
+        ];
+
+        me.columns = Ext.Array.merge(me.columns,
+            [{
+                xtype    : 'datecolumn',
+                text     : 'Last Update',
+                width    : 150,
+                dataIndex: 'last_update',
+                format: 'Y-m-j H:i:s',
+                filter: true
+            },
+            {
+                xtype: 'widgetcolumn',
+                width: 45,
+                sortable: false,
+                menuDisabled: true,
+                widget: {
+                    xtype: 'button',
+                    glyph: Packt.util.Glyphs.getGlyph('destroy')
+                }
+            }]
+        );
+
+        me.callParent(arguments);
+    }
+});

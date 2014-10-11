addaf6617bd0c6e8d969fc05486ce58ec34edb68
Loiane Groner <me@loiane.com>
2014-10-11 10:48:21 -0300

    added updates - chapter 7

--- db-scripts/06-MenuUpdates.sql
+++ db-scripts/06-MenuUpdates.sql
@@ -1,1 +1,7 @@
 UPDATE `sakila`.`menu` SET `className`='user' WHERE `id`='3';
+
+UPDATE `sakila`.`menu` SET `className`='actorsgrid' WHERE `id`='5';
+UPDATE `sakila`.`menu` SET `className`='categoriesgrid' WHERE `id`='6';
+UPDATE `sakila`.`menu` SET `className`='languagesgrid' WHERE `id`='7';
+UPDATE `sakila`.`menu` SET `className`='citiesgrid' WHERE `id`='8';
+UPDATE `sakila`.`menu` SET `className`='countriesgrid' WHERE `id`='9';

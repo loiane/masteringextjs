556039cc6ff737da7a106ee134b16e0c4ec5dbef
Loiane Groner <me@loiane.com>
2014-12-06 15:58:54 -0200

    chapter 2 reviewed

--- app/Application.js
+++ app/Application.js
@@ -70,64 +70,61 @@ Ext.define('Packt.Application', {
     enableQuickTips: true,
 
     views: [
-        // TODO: add views here
     ],
 
     controllers: [
         //'Root',
         'Menu',
         'StaticData'
-        ,'Test'
+        ,'Test'      //TODO: remove this for complete app - this will skip the login page
     ],
 
     stores: [
-        // TODO: add stores here
     ],
 
     defaultToken : 'home',
     
     launch: function () {
 
-//        var me = this;
-//
-//        var task = new Ext.util.DelayedTask(function() {
-//
-//            //Fade out the body mask
-//            me.splashscreen.fadeOut({
-//                duration: 1000,
-//                remove:true
-//            });
-//
-//            //Fade out the icon and message
-//            me.splashscreen.next().fadeOut({
-//                duration: 1000,
-//                remove:true,
-//                listeners: {
-//                    afteranimate: function(el, startTime, eOpts ){
-//                        console.log('launch');
-//                        Ext.widget('login');
-//                    }
-//                }
-//            });
-//        });
+        var me = this;
 
-        Ext.widget('login');
+        var task = new Ext.util.DelayedTask(function() {
 
-        //task.delay(2000);
+            //Fade out the body mask
+            me.splashscreen.fadeOut({
+                duration: 1000,
+                remove:true
+            });
+
+            //Fade out the icon and message
+            me.splashscreen.next().fadeOut({
+                duration: 1000,
+                remove:true,
+                listeners: {
+                    afteranimate: function(el, startTime, eOpts ){
+                        //console.log('launch');
+                        Ext.widget('login');
+                    }
+                }
+            });
+        });
+
+
+        task.delay(2000);
     },
 
     init: function () {
         var me = this;
 
         // Start the mask on the body and get a reference to the mask
-        //me.splashscreen = Ext.getBody().mask('Loading application', 'splashscreen');
+        me.splashscreen = Ext.getBody().mask('Loading application', 'splashscreen');
 
         // Add a new class to this mask as we want it to look different from the default.
-        //me.splashscreen.addCls('splashscreen');
+        me.splashscreen.addCls('splashscreen');
 
         // Insert a new div before the loading icon where we can place our logo.
-//        Ext.DomHelper.insertFirst(Ext.query('.x-mask-msg')[0], {
-//            cls: 'x-splash-icon'
-//        });
+        Ext.DomHelper.insertFirst(Ext.query('.x-mask-msg')[0], {
+            cls: 'x-splash-icon'
+        });
     }
 });

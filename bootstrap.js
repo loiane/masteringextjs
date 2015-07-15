d5d0569788887219a6d704319c39ce8e3d58adf0
Loiane Groner <loianeg@gmail.com>
2015-07-15 14:45:52 -0300

    added header toolbar

--- bootstrap.js
+++ bootstrap.js
@@ -1,5 +1,5 @@
 var Ext = Ext || {};
-Ext.manifest = Ext.manifest || "modern.json";
+Ext.manifest = Ext.manifest || "classic.json";
 // @tag core
 // @define Ext.Boot
 
--- classic/src/view/main/Main.js
+++ classic/src/view/main/Main.js
@@ -6,99 +6,30 @@
  * TODO - Replace this content of this view to suite the needs of your application.
  */
 Ext.define('MasteringExtJS.view.main.Main', {
-    extend: 'Ext.tab.Panel',
+    extend: 'Ext.container.Viewport',
     xtype: 'app-main',
 
     requires: [
-        'Ext.plugin.Viewport',
-        'Ext.window.MessageBox',
-
         'MasteringExtJS.view.main.MainController',
         'MasteringExtJS.view.main.MainModel',
-        'MasteringExtJS.view.main.List'
+        'MasteringExtJS.view.main.Header',
+
+        'Ext.layout.container.Border',
+        'Ext.Container'
     ],
 
     controller: 'main',
     viewModel: 'main',
 
-    ui: 'navigation',
-
-    tabBarHeaderPosition: 1,
-    titleRotation: 0,
-    tabRotation: 0,
-
-    header: {
-        layout: {
-            align: 'stretchmax'
-        },
-        title: {
-            bind: {
-                text: '{name}'
-            },
-            flex: 0
-        },
-        iconCls: 'fa-th-list'
-    },
-
-    tabBar: {
-        flex: 1,
-        layout: {
-            align: 'stretch',
-            overflowHandler: 'none'
-        }
-    },
-
-    responsiveConfig: {
-        tall: {
-            headerPosition: 'top'
-        },
-        wide: {
-            headerPosition: 'left'
-        }
-    },
-
-    defaults: {
-        bodyPadding: 20,
-        tabConfig: {
-            plugins: 'responsive',
-            responsiveConfig: {
-                wide: {
-                    iconAlign: 'left',
-                    textAlign: 'left'
-                },
-                tall: {
-                    iconAlign: 'top',
-                    textAlign: 'center',
-                    width: 120
-                }
-            }
-        }
+    layout: {
+        type: 'border'
     },
 
     items: [{
-        title: 'Home',
-        iconCls: 'fa-home',
-        // The following grid shares a store with the classic version's grid as well!
-        items: [{
-            xtype: 'mainlist'
-        }]
-    }, {
-        title: 'Users',
-        iconCls: 'fa-user',
-        bind: {
-            html: '{loremIpsum}'
-        }
-    }, {
-        title: 'Groups',
-        iconCls: 'fa-users',
-        bind: {
-            html: '{loremIpsum}'
-        }
-    }, {
-        title: 'Settings',
-        iconCls: 'fa-cog',
-        bind: {
-            html: '{loremIpsum}'
-        }
+        region: 'center',
+        xtype: 'container'
+    },{
+        xtype: 'app-header',
+        region: 'north'
     }]
 });

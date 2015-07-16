5330723d07fb1e30ef87be999a3afec74b48519e
Loiane Groner <loianeg@gmail.com>
2015-07-16 09:02:28 -0300

    added menu classes

---
+++ classic/src/view/menulist/MenuList.js
@@ -0,0 +1,18 @@
+Ext.define('MasteringExtJS.view.menulist.MenuList', {
+    extend: 'Ext.Container',
+
+    requires: [
+        'MasteringExtJS.view.menulist.MenuListController'
+    ],
+
+    /*
+    Uncomment to give this component an xtype
+    xtype: 'menulist',
+    */
+
+    controller: 'menulist',
+
+    items: [
+        /* include child components here */
+    ]
+});
---
+++ classic/src/view/menulist/MenuListController.js
@@ -0,0 +1,11 @@
+Ext.define('MasteringExtJS.view.menulist.MenuListController', {
+    extend: 'Ext.app.ViewController',
+    alias: 'controller.menulist',
+
+    /**
+     * Called when the view is created
+     */
+    init: function() {
+
+    }
+});

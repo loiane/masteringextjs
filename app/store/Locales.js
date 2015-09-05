d848c8037fefe8e9f02c0017ba3ec77076d11861
Loiane Groner <loianeg@gmail.com>
2015-09-05 10:35:34 -0300

    added tree view menu

--- app/store/Locales.js
+++ app/store/Locales.js
@@ -22,6 +22,84 @@ Ext.define('MasteringExtJS.store.Locales', {
             en: 'Logout',
             es: 'Salir',
             pt_BR: 'Sair'
+        },
+        {
+            id: 'menu1',
+            en: 'Security',
+            es: '',
+            pt_BR: ''
+        },
+        {
+            id: 'menu11',
+            en: 'Groups and Permissions',
+            es: '',
+            pt_BR: ''
+        },
+        {
+            id: 'menu12',
+            en: 'Users',
+            es: '',
+            pt_BR: ''
+        },
+        {
+            id: 'staticData',
+            en: 'Static Data',
+            es: '',
+            pt_BR: ''
+        },
+        {
+            id: 'actors',
+            en: 'Actors',
+            es: '',
+            pt_BR: ''
+        },
+        {
+            id: 'categories',
+            en: 'Categories',
+            es: '',
+            pt_BR: ''
+        },
+        {
+            id: 'languages',
+            en: 'Languages',
+            es: '',
+            pt_BR: ''
+        },
+        {
+            id: 'cities',
+            en: 'Cities',
+            es: '',
+            pt_BR: ''
+        },
+        {
+            id: 'countries',
+            en: 'Countries',
+            es: '',
+            pt_BR: ''
+        },
+        {
+            id: 'cms',
+            en: 'Content Management',
+            es: '',
+            pt_BR: ''
+        },
+        {
+            id: 'films',
+            en: 'Films',
+            es: '',
+            pt_BR: ''
+        },
+        {
+            id: 'reports',
+            en: 'Reports',
+            es: '',
+            pt_BR: ''
+        },
+        {
+            id: 'salesfilmcategory',
+            en: 'Sales by Film Category',
+            es: '',
+            pt_BR: ''
         }
     ]
 });
--- app/store/MenuList.js
+++ app/store/MenuList.js
@@ -16,7 +16,39 @@ Ext.define('MasteringExtJS.store.MenuList', {
         url: 'data/menu.json',
         reader: {
             type: 'json',
-            rootProperty: 'data'
+            transform: {
+                fn: function(data) {
+
+                    var transfData = [];
+
+                    for (var i=0; i<data.data.length; i++){
+                        var temp = {
+                            id: data.data[i].id,
+                            text: _(data.data[i].text),
+                            iconCls: data.data[i].iconCls,
+                            leaf: false,
+                            //expanded: true,
+                            children: []
+                        };
+
+                        for (var j=0; j<data.data[i].children.length; j++){
+                            temp.children.push({
+                                leaf: true,
+                                id: data.data[i].children[j].id,
+                                text: _(data.data[i].children[j].text),
+                                iconCls: data.data[i].children[j].iconCls,
+                                className: data.data[i].children[j].className
+                            });
+
+                        }
+
+                        transfData.push(temp);
+                    }
+
+                    return transfData;
+                },
+                scope: this
+            }
         }
     }
 });

ed1d083200606e1d02c343a50642a5d398b1adaf
Loiane Groner <loianeg@gmail.com>
2015-09-05 09:21:47 -0300

    added tree view menu

--- app/store/MenuList.js
+++ app/store/MenuList.js
@@ -2,149 +2,21 @@ Ext.define('MasteringExtJS.store.MenuList', {
     extend: 'Ext.data.TreeStore',
 
     storeId: 'MenuList',
-    root: {
-        expanded: true,
-        children: [
-            {
-                text:   'Dashboard',
-                view:   'dashboard.Dashboard',
-                leaf:   true,
-                iconCls: 'right-icon new-icon x-fa fa-desktop',
-                routeId: 'dashboard'
-            },
-            {
-                text: 'Security',
-                expanded: false,
-                selectable: false,
-                iconCls: 'x-fa fa-group',
-                routeId : 'pages-parent',
-                id:       'pages-parent',
-                children: [
-                    {
-                        text: 'Groups',
-                        view: 'user.groups',
-                        leaf: true,
-                        iconCls: 'x-fa fa-file-o',
-                        routeId:'pages.blank'
-                    },
 
-                    {
-                        text: 'Users',
-                        view: 'user.user',
-                        leaf: true,
-                        iconCls: 'x-fa fa-exclamation-triangle',
-                        routeId:'pages.404'
-                    }
-                ]
-            },
-            {
-                text:   'Profile',
-                view:   'profile.UserProfile',
-                leaf:   true,
-                iconCls: 'x-fa fa-user',
-                routeId:'profile'
-            },
-            {
-                text:   'Search results',
-                view:   'search.Results',
-                leaf:   true,
-                iconCls: 'x-fa fa-search',
-                routeId:'search'
-            },
-            {
-                text: 'FAQ',
-                view: 'pages.FAQ',
-                leaf: true,
-                iconCls: 'x-fa fa-question',
-                routeId:'faq'
-            },
-            {
-                text: 'Pages',
-                expanded: false,
-                selectable: false,
-                iconCls: 'x-fa fa-leanpub',
-                routeId : 'pages-parent',
-                id:       'pages-parent',
-                children: [
-                    {
-                        text: 'Blank Page',
-                        view: 'pages.BlankPage',
-                        leaf: true,
-                        iconCls: 'x-fa fa-file-o',
-                        routeId:'pages.blank'
-                    },
-
-                    {
-                        text: '404 Error',
-                        view: 'pages.Error404Window',
-                        leaf: true,
-                        iconCls: 'x-fa fa-exclamation-triangle',
-                        routeId:'pages.404'
-                    },
-                    {
-                        text: '500 Error',
-                        view: 'pages.Error500Window',
-                        leaf: true,
-                        iconCls: 'x-fa fa-times-circle',
-                        routeId:'pages.500'
-                    },
-                    {
-                        text: 'Lock Screen',
-                        view: 'authentication.LockScreen',
-                        leaf: true,
-                        iconCls: 'x-fa fa-lock',
-                        routeId:'authentication.lockscreen'
-                    },
-
-                    {
-                        text: 'Login',
-                        view: 'authentication.Login',
-                        leaf: true,
-                        iconCls: 'x-fa fa-check',
-                        routeId:'authentication.login'
-                    },
-                    {
-                        text: 'Register',
-                        view: 'authentication.Register',
-                        leaf: true,
-                        iconCls: 'x-fa fa-pencil-square-o',
-                        routeId:'authentication.register'
-                    },
-                    {
-                        text: 'Password Reset',
-                        view: 'authentication.PasswordReset',
-                        leaf: true,
-                        iconCls: 'x-fa fa-lightbulb-o',
-                        routeId:'authentication.passwordreset'
-                    }
-                ]
-            },
-            {
-                text:   'Widgets',
-                view:   'widgets.Widgets',
-                leaf:   true,
-                iconCls: 'x-fa fa-flask',
-                routeId:'widgets'
-            },
-            {
-                text:   'Forms',
-                view:   'forms.Wizards',
-                leaf:   true,
-                iconCls: 'x-fa fa-edit',
-                routeId:'forms'
-            },
-            {
-                text: 'Charts',
-                view: 'charts.Charts',
-                iconCls: 'x-fa fa-pie-chart',
-                leaf:   true,
-                routeId:'charts'
-            }
-        ]
-    },
     fields: [
         {
             name: 'text'
         }
-    ]
+    ],
+
+    autoLoad: true,
+
+    proxy: {
+        type: 'ajax',
+        url: 'data/menu.json',
+        reader: {
+            type: 'json',
+            rootProperty: 'data'
+        }
+    }
 });
